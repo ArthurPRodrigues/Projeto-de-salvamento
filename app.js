7ea679942060e7f80692d35b390d0c06871df789
ArthurPR <arthurprodrigues1@gmail.com>
2023-08-10 18:38:36 -0300

    git push

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se concectando ao banco de dados")
+
+var entrada = document.querySelector("input"); 
+
+var frase = "Está errado!";
+
+function mostra(frase) {
+    document.write(frase);
+    return frase;
+}
+
+if (entrada.value !== "0") { 
+    mostra(frase);
+}
